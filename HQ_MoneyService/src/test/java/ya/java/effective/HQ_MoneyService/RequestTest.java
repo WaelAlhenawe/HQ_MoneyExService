ffcd3f632c8727c7569b465214cb18f7e18502c8
Patrik Gustafsson <patrik.e.gustafsson@hotmail.com>
2020-04-30 20:39:59 +0200

    More tests updated

--- HQ_MoneyService/src/test/java/ya/java/effective/HQ_MoneyService/RequestTest.java
+++ HQ_MoneyService/src/test/java/ya/java/effective/HQ_MoneyService/RequestTest.java
@@ -19,137 +19,150 @@ import org.junit.Test;
 
 
 public class RequestTest {
-	
 
-	
-	
+
+
+
 	private static Request  r1 = null;
-	
+
 	private static String site = "ALL";
 	private static  String duration = "DAY";
 	private static  LocalDate date = LocalDate.parse("2020-04-22");
 	private static  String currency = "EUR";
-	
+
 	@BeforeClass
 	public static void setUp() {
-		
+
 		r1 = new Request(site,duration,date,currency);
 	}
-	
+
 	@Test
 	public void testRequest1() {
-		
-		assertNotNull(r1);
+
+		Assert.assertNotNull(r1);
 	}
 
 	@Test
 	public void testRequest2() {
-		
-		assertEquals(r1.getCurrency(),"EUR");
-		
+
+		Assert.assertEquals(r1.getCurrency(),"EUR");
+
 	}
 	@Test
 	public void testRequest3() {
-		
-		assertEquals(r1.getDate(),date);
-		
+
+		Assert.assertEquals(r1.getDate(),date);
+
 	}
-	
+
 	@Test
 	public void testRequest4() {
-		
-		assertEquals(r1.getDuration(),"DAY");
-		
+
+		Assert.assertEquals(r1.getDuration(),"DAY");
+
 	}
-	
+
 	@Test
 	public void testRequest5() {
-		
+
 		LocalDate date1 = LocalDate.parse("2020-04-23");
 		Request r2 = new Request("ALL","WEEK",date1,"GBP");
-		
-		assertNotNull(r2);
-		
+
+		Assert.assertNotNull(r2);
+
 	}
-	
+
 	@Test
 	public void testRequest6() {
-		
+
 		LocalDate date1 = LocalDate.parse("2020-04-23");
 		Request r2 = new Request("ALL","WEEK",date1,"GBP");
-		assertFalse(r2.hashCode() == r1.hashCode());
-		
-		
+		Assert.assertFalse(r2.hashCode() == r1.hashCode());
+
+
 	}
-	
+
 	@Test
 	public void testRequest7() {
-		
+
 		LocalDate date1 = LocalDate.parse("2020-04-23");
 		Request r2 = new Request("ALL","WEEK",date1,"GBP");
 		Request r3 = new Request("ALL","WEEK",date1,"GBP");
-		assertTrue(r2.hashCode() == r3.hashCode());
-			
+		Assert.assertTrue(r2.hashCode() == r3.hashCode());
+
 	}
 	@Test
 	public void testRequest8() {
-		
+
 		LocalDate date1 = LocalDate.parse("2020-04-23");
 		Request r2 = new Request("ALL","",date1,"GBP");
-		assertNotNull(r2);		
+		Assert.assertNotNull(r2);		
 	}
-	
+
 	@Test
 	public void testRequest9() {
-		
+
 		LocalDate date1 = LocalDate.parse("2020-04-23");
 		Request r2 = new Request("ALL","WEEK",date1,"GBP");
-		assertFalse(r2.equals(r1));
-		
-		
+		Assert.assertFalse(r2.equals(r1));
+
+
 	}
-	
+
 	@Test
 	public void testRequest10() {
-		
+
 		LocalDate date1 = LocalDate.parse("2020-04-23");
 		Request r2 = new Request("ALL","WEEK",date1,"GBP");
-		assertFalse(r2.getDuration().equals(r1.getDuration()));
-		
-		
+		Assert.assertFalse(r2.getDuration().equals(r1.getDuration()));
+
+
 	}
-	
+
 	@Test
 	public void testRequest11() {
-		
+
 		LocalDate date1 = LocalDate.parse("2020-04-23");
 		Request r2 = new Request("ALL","WEEK",date1,"GBP");
-		assertFalse(r2.getCurrency().equals(r1.getCurrency()));
-		
-		
+		Assert.assertFalse(r2.getCurrency().equals(r1.getCurrency()));
+
+
 	}
 	@Test
 	public void testRequest12() {
-		
+
 		LocalDate date1 = LocalDate.parse("2020-04-23");
 		Request r2 = new Request("ALL","WEEK",date1,"GBP");
-		assertFalse(r2.getDate().equals(r1.getDate()));
-		
-		
+		Assert.assertFalse(r2.getDate().equals(r1.getDate()));
+
+
 	}
 	
-	
+	@Test
+	public void testRequest13() {
+
+		Request  r2 = new Request("SOUTH",duration,date,currency);
+
+		LocalDate date1 = LocalDate.parse("2020-04-23");
+		Request r3 = new Request("SOUTH","WEEK",date1,"GBP");
 		
+		Assert.assertFalse(r3.getDate().equals(r2.getDate()));
+
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
 
